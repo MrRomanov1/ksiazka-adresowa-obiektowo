cb7190cf2d6f997a9a3101cb005d492852d14133
Piotr Romanczak <piotr.romanczak.programista@gmail.com>
2020-05-24 20:13:04 +0200

    Added option to load people to user's vector while logged in

--- MetodyPomocnicze.cpp
+++ MetodyPomocnicze.cpp
@@ -44,3 +44,24 @@ char MetodyPomocnicze::wybierzOpcjeZMenuUzytkownika() {
 
     return wybor;
 }
+
+
+int MetodyPomocnicze::konwersjaStringNaInt(string liczba)
+{
+    int liczbaInt;
+    istringstream iss(liczba);
+    iss >> liczbaInt;
+
+    return liczbaInt;
+}
+
+string MetodyPomocnicze::pobierzLiczbe(string tekst, int pozycjaZnaku)
+{
+    string liczba = "";
+    while(isdigit(tekst[pozycjaZnaku]) == true)
+    {
+        liczba += tekst[pozycjaZnaku];
+        pozycjaZnaku ++;
+    }
+    return liczba;
+}
