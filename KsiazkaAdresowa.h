fef5ef1c7c79af3f068d7d58bb331682c72111bf
Piotr Romanczak <piotr.romanczak.programista@gmail.com>
2020-05-24 17:16:12 +0200

    Added option to login user

--- KsiazkaAdresowa.h
+++ KsiazkaAdresowa.h
@@ -1,3 +1,6 @@
+#ifndef KSIAZKAADRESOWA_H
+#define KSIAZKAADRESOWA_H
+
 #include <iostream>
 
 #include "UzytkownikMenedzer.h"
@@ -8,9 +11,13 @@ class KsiazkaAdresowa {
 
     UzytkownikMenedzer uzytkownikMenedzer;
 public:
-    KsiazkaAdresowa(string nazwaPlikuZUzytkownikami) : uzytkownikMenedzer(nazwaPlikuZUzytkownikami){
+    KsiazkaAdresowa(string nazwaPlikuZUzytkownikami) : uzytkownikMenedzer(nazwaPlikuZUzytkownikami) {
         uzytkownikMenedzer.wczytajUzytkownikowZPliku();
     };
-        void rejestracjaUzytkownika();
-        void wypiszWszystkichUzytkownikow();
-    };
+
+    void rejestracjaUzytkownika();
+    int logowanieUzytkownika();
+    void wypiszWszystkichUzytkownikow();
+};
+
+#endif
--- UzytkownikMenedzer.h
+++ UzytkownikMenedzer.h
@@ -24,6 +24,7 @@ class UzytkownikMenedzer {
 public:
     UzytkownikMenedzer(string nazwaPlikuZUzytkownikami) : plikZUzytkownikami (nazwaPlikuZUzytkownikami) {};
     void rejestracjaUzytkownika();
+    int logowanieUzytkownika();
     void wypiszWszystkichUzytkownikow();
     void wczytajUzytkownikowZPliku();
 };
